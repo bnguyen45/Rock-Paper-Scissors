65b068750ed7bed4f40526c8734288a878d5acc9
Brandon Nguyen <rmpumaz@gmail.com>
2021-12-09 20:22:15 -0500

    added computer class

--- src/Computer.java
+++ src/Computer.java
@@ -1,3 +1,30 @@
+import java.util.Random;
+
 public class Computer extends Player {
+
+private String choice;
+private int score=0;
+
+
+public String getChoice(){
+    return choice;
+}
+
+public void setChoice(){
+   int[] choiceArray ={1, 2, 3};
+    int randomChoice = new Random().nextInt(choiceArray.length);
+    if (randomChoice == choiceArray[0]){
+        choice = "Rock";
+    }else if(randomChoice == choiceArray[1]){
+        choice = "Paper";
+    }else{
+        choice = "Scissors";
+    }
+
+
+
+
+}
+
 
 }
--- src/GameStart.java
+++ src/GameStart.java
@@ -5,15 +5,14 @@ public class GameStart {
 
         Scanner scanner = new Scanner(System.in);
         String choice = "";
-        String vsChoice="";
-
+        String vsChoice = "";
 
 
         System.out.println("Welcome to Rock, Paper, Scissors!");
         System.out.println("Would you like to play vs another player or computer?");
         vsChoice = scanner.next();
 
-        if (vsChoice.equalsIgnoreCase("player")){
+        if (vsChoice.equalsIgnoreCase("player")) {
             Player player1 = new Player();
             Player player2 = new Player();
 
@@ -23,7 +22,7 @@ public class GameStart {
             System.out.println(player1.getName() + " Choose between `Rock`, `Paper`, or `Scissors`");
             choice = scanner.next();
 
-            while(!"Rock".equalsIgnoreCase(choice) && !"Paper".equalsIgnoreCase(choice) && !"Scissors".equalsIgnoreCase(choice)){
+            while (!"Rock".equalsIgnoreCase(choice) && !"Paper".equalsIgnoreCase(choice) && !"Scissors".equalsIgnoreCase(choice)) {
                 System.out.println("Please enter a valid choice");
                 choice = scanner.next();
             }
@@ -33,36 +32,69 @@ public class GameStart {
             System.out.println(player2.getName() + " Choose between `Rock`, `Paper`, or `Scissors`");
             choice = scanner.next();
 
-            while(!"Rock".equalsIgnoreCase(choice) && !"Paper".equalsIgnoreCase(choice) && !"Scissors".equalsIgnoreCase(choice)){
+            while (!"Rock".equalsIgnoreCase(choice) && !"Paper".equalsIgnoreCase(choice) && !"Scissors".equalsIgnoreCase(choice)) {
                 System.out.println("Please enter a valid choice");
                 choice = scanner.next();
             }
             player2.setChoice(choice);
 
-            if (player1.getChoice().equalsIgnoreCase(player2.getChoice())){
+            if (player1.getChoice().equalsIgnoreCase(player2.getChoice())) {
                 System.out.println("Game is a draw!");
-            }else{
+            } else {
                 Player winner = new GameLogic().start(player1, player2);
                 System.out.println(winner.getName() + " wins the game!");
 
-                if(winner == player1){
+                if (winner == player1) {
                     player1.incrementScore();
 
                     System.out.println("Player 1 Score: " + player1.getScore());
-                }else{
+
+                } else {
                     player2.incrementScore();
                     System.out.println("Player 2 Score: " + player2.getScore());
                 }
             }
 
+        } else {
+            Player player1 = new Player();
+            Computer computer = new Computer();
+
+            player1.setName("Player1");
+            computer.setName("Computer");
+
+            System.out.println(player1.getName() + " Choose between `Rock`, `Paper`, or `Scissors`");
+            choice = scanner.next();
+
+            while (!"Rock".equalsIgnoreCase(choice) && !"Paper".equalsIgnoreCase(choice) && !"Scissors".equalsIgnoreCase(choice)) {
+                System.out.println("Please enter a valid choice");
+                choice = scanner.next();
+            }
+
+            player1.setChoice(choice);
+
+            System.out.println(computer.getName() + " chose " + computer.getChoice());
+
+
+            if (player1.getChoice().equalsIgnoreCase(computer.getChoice())) {
+                System.out.println("Game is a draw!");
+            } else {
+                Player winner = new GameLogic().start(player1, computer);
+                System.out.println(winner.getName() + " wins the game!");
+
+                if (winner == player1) {
+                    player1.incrementScore();
+
+                    System.out.println("Player 1 Score: " + player1.getScore());
+
+                } else {
+                    computer.incrementScore();
+                    System.out.println("Computer Score: " + computer.getScore());
+                }
+
+
+            }
+
 
         }
-
-
-
-
-
-
     }
-
 }
